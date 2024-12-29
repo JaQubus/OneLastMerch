0f8afb9f0d7912608779e59f7a58dc904d6702d4
Kuba <jakubkulik20@gmail.com>
2024-12-29 13:13:33 +0100

    Working on vercel

--- OneLastMerch/OneLastMerch/settings.py
+++ OneLastMerch/OneLastMerch/settings.py
@@ -79,7 +79,14 @@ WSGI_APPLICATION = 'OneLastMerch.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('POSTGRES_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DATABASE', ''),
+        'USER': os.getenv('POSTGRES_USER', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('POSTGRES_HOST', ''),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),  # Default PostgreSQL port
+    }
 }
 
 
