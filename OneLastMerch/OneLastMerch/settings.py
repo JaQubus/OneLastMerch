5125217af8115964c6461f8527ffcc6053d8da90
Kuba <jakubkulik20@gmail.com>
2024-12-29 15:39:54 +0100

    Working on vercel

--- OneLastMerch/OneLastMerch/settings.py
+++ OneLastMerch/OneLastMerch/settings.py
@@ -119,15 +119,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+# ... other settings ...
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' 
 
-# Set the path where collected static files should be stored
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Add the static/icons as folder os static
+    os.path.join(BASE_DIR, 'static'), 
 ]
 
 # Default primary key field type
