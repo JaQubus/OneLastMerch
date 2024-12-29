2fd78af74c3dd1670cd8451582e95e584a4a62ec
Kuba <jakubkulik20@gmail.com>
2024-12-29 16:34:56 +0100

    Working on vercel

--- OneLastMerch/OneLastMerch/settings.py
+++ OneLastMerch/OneLastMerch/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-365fu+ss@yn(9+eyi3brr85erd__qrzoiaof9go&ggk^3fjxkb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
 
 # Application definition
