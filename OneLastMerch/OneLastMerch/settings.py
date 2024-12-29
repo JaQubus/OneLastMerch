69a54626e5a09a6e951fcf2e97dcc58fd4293221
Kuba <jakubkulik20@gmail.com>
2024-12-29 16:15:52 +0100

    Working on vercel

--- OneLastMerch/OneLastMerch/settings.py
+++ OneLastMerch/OneLastMerch/settings.py
@@ -37,7 +37,6 @@ ALLOWED_HOSTS = [".vercel.app", ".now.sh", "localhost", "127.0.0.1"]
 INSTALLED_APPS = [
     'auth',
     'utils',
-    "whitenoise_runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,7 +47,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
