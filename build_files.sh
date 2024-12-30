ad3d763d783be467d05118a564a49503969d102f
Kuba <jakubkulik20@gmail.com>
2024-12-30 14:58:21 +0100

    THE SHIT ACTUALLY WORKS (ALMOST) ALL FILES IN THE OUTPUT DIRECTORY

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install Python dependencies
-# python3 -m pip install -r OneLastMerch/requirements.txt
-# python3 OneLastMerch/manage.py collectstatic --noinput
+python3 -m pip install -r OneLastMerch/requirements.txt
+python3 OneLastMerch/manage.py collectstatic --noinput
 
