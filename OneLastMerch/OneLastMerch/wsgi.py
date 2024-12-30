7a9796d98f0efbdf376bb9f963252e3bec228fa6
Kuba <jakubkulik20@gmail.com>
2024-12-30 15:01:21 +0100

    Changed Vercel settings, added developement command

--- OneLastMerch/OneLastMerch/wsgi.py
+++ OneLastMerch/OneLastMerch/wsgi.py
@@ -12,7 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OneLastMerch.settings')
-
 application = get_wsgi_application()
 # add this vercel variable
 app = application
