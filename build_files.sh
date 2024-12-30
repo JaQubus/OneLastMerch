aed0c95bebdaee6cceeb4cdd7774fe76afe36640
Kuba <jakubkulik20@gmail.com>
2024-12-30 14:36:52 +0100

    Working on vercel

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,4 @@
 
 # Install Python dependencies
 python3 -m pip install -r OneLastMerch/requirements.txt
-
-# Collect static files into the 'OneLastMerch/staticfiles' directory
 python3 OneLastMerch/manage.py collectstatic --noinput
