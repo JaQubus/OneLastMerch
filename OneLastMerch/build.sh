ecd6552fe965377d9d52754ae853e692679e9388
Kuba <jakubkulik20@gmail.com>
2025-01-09 18:28:04 +0100

    trying to host on render. Added render.yaml from docs, changed settings accordingly

--- OneLastMerch/build.sh
+++ OneLastMerch/build.sh
@@ -2,10 +2,10 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+pip install -r OneLastMerch/requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python OneLastMerch/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python OneLastMerch/manage.py migrate
