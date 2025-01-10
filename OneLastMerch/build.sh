ae08525b6a9df3e845517ebcac1abb3972cd0e61
Kuba <jakubkulik20@gmail.com>
2025-01-10 18:22:29 +0100

    Not giving up. Made the build.sh change directories, so no more module not found errors occure

--- OneLastMerch/build.sh
+++ OneLastMerch/build.sh
@@ -1,11 +1,13 @@
 # Exit on error
 set -o errexit
 
+cd OneLastMerch/
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r OneLastMerch/requirements.txt
+pip install -r requirements.txt
 
 # Convert static asset files
-python OneLastMerch/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python OneLastMerch/manage.py migrate
+python manage.py migrate
